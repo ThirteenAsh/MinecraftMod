fee059297aa6f8d988139194701daf48beb267d5
ThirteenAsh <1984885454@qq.com>
2025-09-11 13:01:35 +0800

    info

--- src/main/java/com/besson/thirteenash/items/ModItems.java
+++ src/main/java/com/besson/thirteenash/items/ModItems.java
@@ -1,36 +1,41 @@
 package com.besson.thirteenash.items;
 
 import com.besson.thirteenash.TestMod;
-import net.minecraft.item.BlockItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.item.FoodComponent;
 
 public class ModItems {
+
+    // 定义樱桃的食物属性
+    public static final FoodComponent CHERRY_FOOD = new FoodComponent.Builder()
+            .hunger(3)                  // 恢复饥饿值 4
+            .saturationModifier(0.3f)   // 饱和度 0.3
+            .build();
+
     // 示例模组
     public static  final Item EXAMPLE_ITEM = registerItems("example_item",
             new Item(new Item.Settings().maxCount(64)));
+    public static  final Item CHERRY = registerItems("cherry",
+            new Item(new Item.Settings().maxCount(64)
+                    .maxCount(64)            // 最大堆叠 64 个
+                    .food(CHERRY_FOOD)   // 设置食物属性));
+            ));
 
-    public static Item registerItems(String id, Item Item){
-        return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), new Identifier(TestMod.MOD_ID, id)), Item);
+    // 基础注册方法
+    public static Item registerItems(String id, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TestMod.MOD_ID, id), item);
     }
 
-    public static Item register(String id, Item item) {
-        return register(new Identifier(TestMod.MOD_ID, id), item);
-    }
+    private static void addItemtoItemGroup(FabricItemGroupEntries entries){
+        entries.add(EXAMPLE_ITEM);
+        entries.add(CHERRY);
 
-    public static Item register(Identifier id, Item item) {
-        return register(RegistryKey.of(Registries.ITEM.getKey(), id), item);
-    }
-
-    public static Item register(RegistryKey<Item> key, Item item) {
-        if (item instanceof BlockItem) {
-            ((BlockItem)item).appendBlocks(Item.BLOCK_ITEMS, item);
-        }
-
-        return Registry.register(Registries.ITEM, key, item);
     }
 
     public static void registerItems(){
